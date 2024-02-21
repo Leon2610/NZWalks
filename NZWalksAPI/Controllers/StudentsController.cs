e7db506bd2907813c1cb330b6c30deb3fc530d70
Leonel Leon <leonleo1996@gmail.com>
2024-02-20 20:47:15 -0600

    Agregar archivos de proyecto.

---
+++ NZWalksAPI/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPI.Controllers
+{
+    //https://localhost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentsNames = new string[] { "Tom", "Jerry", "Donald" };
+
+            return Ok(studentsNames);
+        }
+    }
+}
