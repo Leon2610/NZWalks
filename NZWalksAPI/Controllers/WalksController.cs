f5e33f5832437852937ee82c90c84b217ee0d292
Leonel Leon <leonleo1996@gmail.com>
2024-02-23 22:42:50 -0600

    Creating walks controller and create new walk action

---
+++ NZWalksAPI/Controllers/WalksController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Models.Domain;
+using NZWalksAPI.Models.DTO;
+using NZWalksAPI.Repositories;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IWalkRepository walkRepository;
+
+        public WalksController(IMapper mapper, IWalkRepository walkRepository)
+        {
+            this.mapper = mapper;
+            this.walkRepository = walkRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddWalkRequestDTO addWalkRequestDTO)
+        {
+            var walkDomainModel = mapper.Map<Walk>(addWalkRequestDTO);
+
+            await walkRepository.CreateAsync(walkDomainModel);
+
+            var walkDTO = mapper.Map<WalkDTO>(walkDomainModel);
+
+            return Ok(walkDTO);
+        }
+    }
+}
